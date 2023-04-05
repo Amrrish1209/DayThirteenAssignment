c39aba9a10485f8bdd30f4dd409150a39f724ef0
Amrrish1209 <roshanbabubl@gmail.com>
2023-04-05 15:14:09 +0530

    Add: Refactor all the 3 to One Generic Method and find the maximum

--- src/com/bridgelabz/genericstestmaximum/MaximumNumbers.java
+++ src/com/bridgelabz/genericstestmaximum/MaximumNumbers.java
@@ -2,45 +2,44 @@ package com.bridgelabz.genericstestmaximum;
 
 public class MaximumNumbers {
 
-	public static void sMax(String x, String y, String z) {
-		String max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
-		System.out.println(max);
-	}
+	/*
+	 * public static void sMax(String x, String y, String z) { String max = x; if
+	 * (y.compareTo(max) > 0) { max = y; } if (z.compareTo(max) > 0) { max = z; }
+	 * System.out.println(max); }
+	 * 
+	 * public static void fMax(double d, double e, double f) { Double max = d; if
+	 * (Double.compare(e, max) > 0.0) { max = e; } if (Double.compare(f, max) > 0.0)
+	 * { max = f; } System.out.println(max); }
+	 * 
+	 * public static void iMax(int a, int b, int c) {
+	 * 
+	 * int max = a;
+	 * 
+	 * if (Integer.compare(b, max) > 0) { max = b; } if (Integer.compare(c, max) >
+	 * 0) { max = c; } System.out.println(max); }
+	 */
 
-	public static void fMax(double d, double e, double f) {
-		Double max = d;
-		if (Double.compare(e, max) > 0.0) {
-			max = e;
-		}
-		if (Double.compare(f, max) > 0.0) {
-			max = f;
-		}
-		System.out.println(max);
-	}
+	public static <M extends Comparable<M>> M maximum(M a, M b, M c) {
 
-	public static void iMax(int a, int b, int c) {
+		M max = a;
 
-		int max = a;
-
-		if (Integer.compare(b, max) > 0) {
+		if (b.compareTo(max) > 0) {
 			max = b;
 		}
-		if (Integer.compare(c, max) > 0) {
+		if (c.compareTo(max) > 0) {
 			max = c;
 		}
-		System.out.println(max);
+		return max;
 	}
 
 	public static void main(String[] args) {
 
-		iMax(20, 30, 10);
-		fMax(2.5, 1.0, 5.0);
-		sMax("apple", "banana", "orange");
+		/*
+		 * iMax(20, 30, 10); fMax(2.5, 1.0, 5.0); sMax("apple", "banana", "orange");
+		 */
+
+		System.out.println(MaximumNumbers.maximum(10, 20, 30));
+		System.out.println(MaximumNumbers.maximum(2.5, 5.5, 1.5));
+		System.out.println(MaximumNumbers.maximum("apple", "orange", "grape"));
 	}
 }
