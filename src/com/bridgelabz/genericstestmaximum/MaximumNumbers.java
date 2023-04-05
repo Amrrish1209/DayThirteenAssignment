c6aff69923c05390dfd71a3f8f39bab4ae5888cf
Amrrish1209 <roshanbabubl@gmail.com>
2023-04-05 16:22:57 +0530

    Add:Extend the max method to take more then three parameters

--- src/com/bridgelabz/genericstestmaximum/MaximumNumbers.java
+++ src/com/bridgelabz/genericstestmaximum/MaximumNumbers.java
@@ -5,17 +5,19 @@ public class MaximumNumbers<M extends Comparable<M>> {
 	private M a;
 	private M b;
 	private M c;
-	private M max;
+	private M d;
 
-	public MaximumNumbers(M a, M b, M c) {
+	public MaximumNumbers(M a, M b, M c, M d) {
 		super();
 		this.a = a;
 		this.b = b;
 		this.c = c;
-		this.max = a;
+		this.d = d;
 	}
 
 	public M max() {
+
+		M max = a;
 
 		if (b.compareTo(max) > 0) {
 			max = b;
@@ -23,14 +25,17 @@ public class MaximumNumbers<M extends Comparable<M>> {
 		if (c.compareTo(max) > 0) {
 			max = c;
 		}
+		if (d.compareTo(max) > 0) {
+			max = d;
+		}
 		return max;
 	}
 
 	public static void main(String[] args) {
 
-		MaximumNumbers<Integer> intMax = new MaximumNumbers<Integer>(50, 10, 30);
-		MaximumNumbers<Double> doubleMax = new MaximumNumbers<Double>(3.5, 1.5, 1.0);
-		MaximumNumbers<String> stringMax = new MaximumNumbers<String>("apple", "orange", "grape");
+		MaximumNumbers<Integer> intMax = new MaximumNumbers<Integer>(50, 10, 30, 20);
+		MaximumNumbers<Double> doubleMax = new MaximumNumbers<Double>(3.5, 1.5, 1.0, 5.4);
+		MaximumNumbers<String> stringMax = new MaximumNumbers<String>("apple", "peach", "banana", "orange");
 
 		System.out.println(intMax.max());
 		System.out.println(doubleMax.max());
